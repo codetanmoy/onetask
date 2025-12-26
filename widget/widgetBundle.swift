177a2c7d702da6816099ded6c760ea0905043599
codetanmoy <tanmoykhanra@icloud.com>
2025-12-26 19:31:19 +0530

    widget before that

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Tanmoy Khanra on 26/12/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
